891261d9dcf7160c67e7e880ffcbe04d6e68a05b
DolevMishali <dolev.mishali@gmail.com>
2024-02-12 16:10:37 +0200

    Comment was added

--- models/Worker/workerModel.js
+++ models/Worker/workerModel.js
@@ -4,6 +4,7 @@ const workerSchema = new mongoose.Schema({
   // id
   name: {type: String, required:true },
   workerNum: { type: Number , required:true },
+  // Add report list - to know about how much "PAKAOT" each worker work on.
 });
 
 module.exports = mongoose.model('Worker', workerSchema);
