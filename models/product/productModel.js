150e57e293e5cabfec151c7dffac801077d029a0
DolevMishali <dolev.mishali@gmail.com>
2024-02-12 15:02:30 +0200

    Product model was added

---
+++ models/product/productModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  productCatalogNum: { type: Number, required: true },
+  amount: { type: Number },
+  partsList: [{ type: Schema.Types.ObjectId, ref: 'Part' }], // Referencing Part schema
+});
+
+module.exports = mongoose.model('Product', productSchema);
