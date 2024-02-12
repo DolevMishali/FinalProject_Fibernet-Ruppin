c7e2b186e83d42da87e730045a366534244eb36a
DolevMishali <dolev.mishali@gmail.com>
2024-02-12 14:31:33 +0200

    Name added to the scheme

--- models/Parts/partModel.js
+++ models/Parts/partModel.js
@@ -4,6 +4,7 @@ const partSchema = new mongoose.Schema({
   // id
   catalogNum: { type: Number, required:true },
   count: { type: Number, required:true },
+  name: { type: String, required:true },
 });
 
 module.exports = mongoose.model('Part', partSchema);
