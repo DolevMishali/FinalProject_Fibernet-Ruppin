fb6ffdc0addb7f384dabf84f6022d29bbc1bb279
DolevMishali <dolev.mishali@gmail.com>
2024-02-12 16:09:21 +0200

    Line of name field changed

--- models/Parts/partModel.js
+++ models/Parts/partModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const partSchema = new mongoose.Schema({
   // id
   partCatalogNum: { type: Number, required:true },
-  amount: { type: Number, required:true },
   name: { type: String, required:true },
+  amount: { type: Number, required:true },
 });
 
 module.exports = mongoose.model('Part', partSchema);
