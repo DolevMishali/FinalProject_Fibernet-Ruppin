321cada52797607237ad819ac49415a1d02b457c
DolevMishali <dolev.mishali@gmail.com>
2024-02-12 11:50:15 +0200

    Added ignoring from versionkey

--- models/reports/reportModel.js
+++ models/reports/reportModel.js
@@ -4,6 +4,10 @@ const reportSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, default: Date.now },
+  status: { type: Boolean, required: true }
+}, {
+  versionKey: false  // Disables the __v field
 });
+
 
 module.exports = mongoose.model('Report', reportSchema);
